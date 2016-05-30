2e54ec34af74c4327fff8cba4673a85c37c090c3
Edward D'Amato <info@edwarddamato.com>
2016-05-30 01:55:36 +0100

    work

---
+++ RockPaperScissors/Gruntfile.js
@@ -0,0 +1,31 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'lib/css/main.css': 'assets/css/main.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass']);
+};
